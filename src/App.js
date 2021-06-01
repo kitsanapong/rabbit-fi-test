e3a27b60b0de965dd7b7306eaf3e806480e9a27c
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2021-06-01 14:48:33 +0700

    add mui theme

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import Desktop from './components/desktop/component'
 import Mobile from './components/mobile/component'
 
 import './App.scss';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff9000',
+      contrastText: '#fff',
+    },
+    secondary: { main: '#0066cc'},
+  },
+});
+
 function App() {
   const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 601px)'})
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 600px)' })
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <div className="App">
-        {isDesktopOrLaptop && <Desktop/>}
-        {isTabletOrMobile && <Mobile/>}
-      </div>
-    </MuiPickersUtilsProvider>
+      <ThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <div className="App">
+          {isDesktopOrLaptop && <Desktop/>}
+          {isTabletOrMobile && <Mobile/>}
+        </div>
+      </MuiPickersUtilsProvider>
+      </ThemeProvider>
   );
 }
 
