41692ccf1daba912a3a026cc6270eeb80a71babd
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2021-06-01 12:32:55 +0700

    mock product select

--- src/components/desktop/component.jsx
+++ src/components/desktop/component.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 import './desktop.scss'
+
+const SelectProduct = () => {
+  const [product, setProduct] = useState()
+  return (
+    <Grid container direction="row" alignItems="center">
+      <Grid item xs={3}>
+        <Typography variant="button" display="block" gutterBottom>Product</Typography>
+      </Grid>
+      <Grid item xc={9}>
+        <FormControl variant="outlined" size="small">
+          <Select
+            id="select-product"
+            value={product}
+            onChange={setProduct}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Flyer - One Sided</MenuItem>
+            <MenuItem value={20}>Flyer - Two Sided</MenuItem>
+            <MenuItem value={30}>Brochure - 4 Page</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+    </Grid>
+  )
+}
 
 const Desktop = () => {
   return (
     <div className="desktop">
-      This is Desktop
+      <Grid container direction="column" justify="flex-start" alignItems="flex-start">
+        <SelectProduct/>
+      </Grid>
     </div>
   )
 }
