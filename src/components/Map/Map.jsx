91082080d10cab8a4bb4d2dc09a93efb24a85d4b
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2021-06-02 02:40:09 +0700

    modal to show location info in map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import GoogleMapReact from 'google-map-react';
 import Modal from '@material-ui/core/Modal';
 
@@ -7,21 +7,52 @@ import Icon from '@material-ui/core/Icon';
 
 import './Map.scss'
 
-const LocationPin = ({
-  text = '',
+const Location = ({
+  data = {},
   onClick = () => {},
 }) => {
-  return (
-    <Grid
-      className="location-pickup"
-      container direction="column"
-      onClick={() => { onClick() }}
-      style={{ cursor: 'pointer' }}
-    >
-      <div style={{ color: 'white', fontSize: 12, backgroundColor: '#ff9000', textAlign: 'center', borderRadius: 3, padding: '2px 4px', fontWeight: '700' }}>{text}</div>
-      <Icon style={{ color: '#ff9000', fontSize: 24 }}>place</Icon>
-    </Grid>
-  )
+  const [showInfo, setShowInfo] = useState(false)
+  if (showInfo) {
+    return (
+      <Grid 
+        direction="column"
+        style={{
+          width: 100,
+          height: 60,
+          backgroundColor: '#ff9000',
+          borderRadius: 5,
+          padding: 4,
+          color: 'white',
+          fontWeight: 700,
+          boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
+        }}
+      >
+        <Grid item style={{ fontSize: 14 }}>{data.name}</Grid>
+        <Grid item >Max Units: {data.max_dist}</Grid>
+        <Grid item>Fee: {data.fee}</Grid>
+        <Grid
+          container
+          justify="flex-end"
+          style={{ fontSize: 14, cursor: 'pointer' }}
+          onClick={() => { onClick() }}
+        >Add</Grid>
+      </Grid>
+    )
+  } else {
+    return (
+      <Grid
+        className="location-pickup"
+        container direction="column"
+        onClick={() => {
+          setShowInfo(true)
+        }}
+        style={{ cursor: 'pointer' }}
+      >
+        <div style={{ color: 'white', fontSize: 12, backgroundColor: '#ff9000', textAlign: 'center', borderRadius: 3, padding: '2px 4px', fontWeight: '700' }}>{data.name}</div>
+        <Icon style={{ color: '#ff9000', fontSize: 24 }}>place</Icon>
+      </Grid>
+    )
+  }
 }
 
 const Map = ({
@@ -48,11 +79,11 @@ const Map = ({
         >
           {data.map((location) => {
             return (
-              <LocationPin
+              <Location
                 key={location.id}
                 lat={location.lat}
                 lng={location.long}
-                text={location.name}
+                data={location}
                 onClick={() => {
                   onClose()
                   onSelect(location)
