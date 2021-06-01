53edb485d82c4c5e78a27d4203fa6f7adbd03408
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2021-06-01 12:40:20 +0700

    bind state with SelectProduct

--- src/components/desktop/component.jsx
+++ src/components/desktop/component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -9,8 +9,8 @@ import Select from '@material-ui/core/Select';
 
 import './desktop.scss'
 
-const SelectProduct = () => {
-  const [product, setProduct] = useState()
+const SelectProduct = ({ state = [] }) => {
+  const [product, setProduct] = state
   return (
     <Grid container direction="row" alignItems="center">
       <Grid item xs={3}>
@@ -21,11 +21,9 @@ const SelectProduct = () => {
           <Select
             id="select-product"
             value={product}
-            onChange={setProduct}
+            onChange={(e) => { setProduct(e.target.value) }}
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
+            <MenuItem value={-1}>Select product</MenuItem>
             <MenuItem value={10}>Flyer - One Sided</MenuItem>
             <MenuItem value={20}>Flyer - Two Sided</MenuItem>
             <MenuItem value={30}>Brochure - 4 Page</MenuItem>
@@ -37,10 +35,11 @@ const SelectProduct = () => {
 }
 
 const Desktop = () => {
+  const productState = useState(-1)
   return (
     <div className="desktop">
       <Grid container direction="column" justify="flex-start" alignItems="flex-start">
-        <SelectProduct/>
+        <SelectProduct state={productState}/>
       </Grid>
     </div>
   )
