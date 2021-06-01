d5b7f43cc6fca447b6767a4fe09516f920c00604
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2021-06-02 03:34:44 +0700

    compelte test for calculation.s

--- src/utils/calculations.test.js
+++ src/utils/calculations.test.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 import CalUtils from './calculations'
 
 describe('maxUnits', () => {
@@ -16,5 +18,35 @@ describe('maxUnits', () => {
     }
     const unit = CalUtils.maxUnits(availableUnits, location)
     expect(unit).toEqual(4000)
+  })
+})
+
+describe('maxProductionUnits', () => {
+  it('should return selected date max_production', () => {
+    const product = {
+      "id": "1",
+      "name": "Flyer - One Sided",
+      "max_production": {
+        "1": 5000,
+        "2": 8000,
+        "3": 12000,
+      },
+      "price_per_unit": 0.01,
+    }
+    
+    let result = CalUtils.maxProductionUnits(product, moment().add(1, 'd'))
+    expect(result).toEqual(5000)
+    result = CalUtils.maxProductionUnits(product, moment().add(2, 'd'))
+    expect(result).toEqual(8000)
+    result = CalUtils.maxProductionUnits(product, moment().add(3, 'd'))
+    expect(result).toEqual(12000)
+    result = CalUtils.maxProductionUnits(product, moment().add(4, 'd'))
+    expect(result).toEqual(12000)
+    result = CalUtils.maxProductionUnits(product, moment().add(5, 'd'))
+    expect(result).toEqual(12000)
+    result = CalUtils.maxProductionUnits(product, moment().add(6, 'd'))
+    expect(result).toEqual(12000)
+    result = CalUtils.maxProductionUnits(product, moment().add(7, 'd'))
+    expect(result).toEqual(12000)
   })
 })
